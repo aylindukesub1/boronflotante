73135cf27e51e5cb790ccaa9ebba1c0ed29fd135
AYLIN_DUQUE_HERRERA <a.20308051281244@cbtis128.edu.mx>
2023-05-22 07:59:24 +0000

    boton flotante

---
+++ lib/screens/boton_flotante.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+//Crea la clase y el estado a cambiar
+class BotonFlotante extends StatefulWidget {
+  const BotonFlotante({super.key});
+  _BotonFlotante createState() => _BotonFlotante();
+}
+//Crear los widgets a mostrar
+
+class _BotonFlotante extends State<BotonFlotante> {
+  String txt = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boton_Flotante'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text(txt)],
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.visibility),
+            onPressed: () => setState(() => txt = "Hola Mundo"),
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.visibility_off),
+            onPressed: () => setState(() => txt = ""),
+          ),
+        ],
+      ),
+    );
+  }
+}
